3f6a091ac6e236f0594781f2614f200be8731f0e
anon000 <33451517+anon000@users.noreply.github.com>
2017-11-08 16:42:44 +0000

    setting up menus
    * Built main menu scene with start game, how to play, settings and quit game
    buttons.
    * Start game and quit game are the only working buttons, start game
    loads the main scene while quit game close the game.
    * Built game over scene with new game and quite game buttons
    * New game loads the main scene while quit game closes the game.
    * All the menus are extremely sketchyare extremely sketchyare extremely
    sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchyare extremely sketchy.......................................................................

---
+++ Assets/Scripts/ManagerMenuBtn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ManagerMenuBtn : MonoBehaviour 
+{
+	public void StartGameBtn (string FirstWave)
+	{
+		SceneManager.LoadScene (FirstWave);
+	}
+
+    public void CloseGameBtn ()
+    {
+        Application.Quit();
+    }
+
+}
