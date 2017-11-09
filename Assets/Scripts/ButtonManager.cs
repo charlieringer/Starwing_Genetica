fef546ec02b9372f15e9916e2c6784b23bcb4603
anon000 <33451517+anon000@users.noreply.github.com>
2017-11-09 17:41:54 +0000

    THRUSTER/FRONT END LOGIC/EXPLOSION
    *Created the particle effects for the thruster
    *Finished with the front end logic
    *Started skatching the particle effect for the explosion

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour 
+{
+	public void LoadScene (string Scene)
+	{
+		SceneManager.LoadScene (Scene);
+	}
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+}
