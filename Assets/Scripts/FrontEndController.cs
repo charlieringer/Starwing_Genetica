e96d70d1d2b693375e0a7dbd6e43aaf70ae59c75
Charlie <charlie@cgs-macd-16893.campus.goldsmiths.ac.uk>
2017-11-15 15:36:57 +0000

    intergrating the choose a ship screen with the rest of the game.

--- Assets/Scripts/FrontEndController.cs
+++ Assets/Scripts/FrontEndController.cs
@@ -14,7 +14,7 @@ public class FrontEndController : MonoBehaviour {
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Return))
 			{
-			SceneManager.LoadScene ("Main Scene");
+			SceneManager.LoadScene ("ChooseShip");
 			}
 	}
 }
