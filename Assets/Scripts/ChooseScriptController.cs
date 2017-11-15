6e1113e6fe73f60cd5e17f7faac2d887d86f5245
Charlie <charlie@cgs-macd-16893.campus.goldsmiths.ac.uk>
2017-11-15 15:30:35 +0000

    Adding a choose ship menu. (currently it is a dummy menu, making it impact the game is next).

---
+++ Assets/Scripts/ChooseScriptController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseScriptController : MonoBehaviour {
+
+	int currentSelection = 1;
+	GameObject[] selectionHighlights = new GameObject[3];
+	public GameObject selection1;
+	public GameObject selection2;
+	public GameObject selection3;
+
+	// Use this for initialization
+	void Awake () {
+		selectionHighlights [0] = selection1;
+		selectionHighlights [1] = selection2;
+		selectionHighlights [2] = selection3;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && currentSelection != 0) {
+			selectionHighlights[currentSelection].SetActive(false);
+			currentSelection--;
+			selectionHighlights[currentSelection].SetActive(true);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && currentSelection != 2) {
+			selectionHighlights[currentSelection].SetActive(false);
+			currentSelection++;
+			selectionHighlights[currentSelection].SetActive(true);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			SceneManager.LoadScene ("Main Scene");
+		}
+	}
+		
+}
